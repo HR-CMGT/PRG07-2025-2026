5bff6f38d5b47be1908ea9070ed76f7dd3e19aa1
Bas Boot <github@basboot.nl>
2026-03-18 08:02:08 +0100

    Add fetch puzzle

--- assets/puzzels.js
+++ assets/puzzels.js
@@ -8,7 +8,6 @@
 // console.log(getRank(1500)); // "Legendary"
 // console.log(getRank(500)); // "Beginner"
 
-
 // Puzzel 2: Nullish Coalescing Operator
 // Schrijf de function getPlayerName die de name van de player moet retourneren.
 // Als name niet bestaat, moet "Unknown Player" geretourneerd worden.
@@ -19,7 +18,6 @@
 // console.log(getPlayerName({name: "ShadowNinja"})); // "ShadowNinja"
 // console.log(getPlayerName({rank: "Legendary"})); // "Unknown Player"
 
-
 // Puzzel 3: Optional Chaining
 // Schrijf de function getWeaponDamage die van een character het damage-veld uit weapon retourneert.
 // Voorkom dat er foutmeldingen ontstaan wanneer player niet de juiste velden bevat.
@@ -29,7 +27,6 @@
 // console.log("\n-- Puzzel 3 --");
 // console.log(getWeaponDamage({name: "Archer", weapon: {damage: 50}})); // 50
 // console.log(getWeaponDamage({name: "Mage"})); // undefined
-
 
 // Puzzel 4: Spread Operator
 // Schrijf de function updateCharacter die een character bijwerkt met nieuwe eigenschappen.
@@ -44,7 +41,6 @@
 // console.log(updateCharacter({name: "Rogue", speed: 30}, {speed: 40, stealth: true}));
 // // { name: "Rogue", speed: 40, stealth: true }
 
-
 // Puzzel 5: Rest Parameters
 // Schrijf de function calculateTotalXP die alle XP-waarden optelt. Let op: de function verwacht maar 1 parameter.
 
@@ -53,7 +49,6 @@
 // console.log("\n-- Puzzel 5 --");
 // console.log(calculateTotalXP(100, 200, 300)); // 600
 // console.log(calculateTotalXP(50, 80, 30, 25, 135, 170, 5, 15)); // 510
-
 
 // Puzzel 6: Destructuring Arrays
 // Schrijf de function createGame die een array ontvangt met gegevens over de game.
@@ -65,7 +60,6 @@
 // console.log("\n-- Puzzel 6 --");
 // console.log(createGame(["Battle Royale", 100, "Shooter"])); // { gameName: "Battle Royale", maxPlayers: 100, genre: "Shooter" }
 // console.log(createGame(["Fantasy Quest", 4, "RPG"])); // { gameName: "Fantasy Quest", maxPlayers: 4, genre: "RPG" }
-
 
 // Puzzel 7: Destructuring Objects
 // Schrijf de function describeEnemy die een omschrijving van de enemy geeft op basis van het object dat wordt meegegeven.
@@ -80,7 +74,6 @@
 // console.log(describeEnemy({name: "Dragon", hp: 500, specialAbility: "breathe fire"}));
 // // "The enemy Dragon has 500 health points and can breathe fire."
 
-
 // Puzzel 8: Array Iteration
 // Schrijf de function doubleDamage die een array verwacht met damages van attacks en vervolgens
 // een array retourneert waar alle aanvalsschade is verdubbeld.
@@ -90,7 +83,6 @@
 // console.log("\n-- Puzzel 8 --");
 // console.log(doubleDamage([10, 20, 30])); // [20, 40, 60]
 // console.log(doubleDamage([5, 15, 25])); // [10, 30, 50]
-
 
 // Puzzel 9: Object Iteration
 // Schrijf de function inventoryList die een object omzet naar een array van [item, quantity] paren.
@@ -103,3 +95,9 @@
 //
 // console.log(inventoryList({ arrow: 50, bow: 1, food: 10 }));
 // // [["arrow", 50], ["bow", 1], ["food", 10]]
+
+// Puzzel 10: Try/catch Await/Async Fetch
+// Schrijf de functie getSmileys die een lijst met alle smiley-namen ophaalt van: https://raw.githubusercontent.com/cheatsnake/emojihub/refs/heads/master/emojistore/data/emojibase.json
+
+// console.log(getSmileys({ sword: 1, potion: 5, shield: 2 }));
+// ["grinning face"], ["grinning face with smiling eyes"], ["face with tears of joy"], ...
